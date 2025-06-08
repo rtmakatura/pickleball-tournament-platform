b4f526fba23907e231aec1df42fb306188be06d5
Ryan Makatura <ryan.makatura@adcogov.org>
2025-06-07 23:48:35 -0600

    Fix: Remove non-existent component exports from UI index - Module 1 Core UI Components complete

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -6,8 +6,8 @@ export { default as Button } from './Button';
 export { default as Input } from './Input';
 export { default as Select } from './Select';
 export { default as Table, TableActions } from './Table';
-export { default as Card, CardGrid, StatCard } from './Card';
-export { default as Alert, AlertList } from './Alert';
+export { default as Card, CardGrid } from './Card';
+export { default as Alert } from './Alert';
 
 // Usage Examples:
 // import { Button, Modal, Input } from '../ui';
